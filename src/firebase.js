ad74a9b630a201590ce07d2af80adf7b4c353e82
sahilshub <sahilsmohamed753@gmail.com>
2023-09-16 17:38:36 +0530

    c1

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBEvR-oeyUMDsGNVItgeFn9heVg3ZjM1J8",
+  authDomain: "netflix-clone-703c8.firebaseapp.com",
+  projectId: "netflix-clone-703c8",
+  storageBucket: "netflix-clone-703c8.appspot.com",
+  messagingSenderId: "94833949344",
+  appId: "1:94833949344:web:af496a56332da00d90e7c5",
+};
